60397297c79955a839bad4f31dc98e614576a546
arunsan2109 <arunsan2109@gmail.com>
2019-04-07 00:40:31 +0530

    message

---
+++ data/propertyFile.js
@@ -0,0 +1,13 @@
+selectors = {
+    urlNightwatch: 'http://nightwatchjs.org/',
+
+    // Home Page Elements
+    homePageTitle: 'Nightwatch.js | Node.js powered End-to-End testing framework',
+    homeNightwatch: 'div.container.navigation:nth-child(2) nav.navbar.navbar-default.navbar-fixed-top div.container div.navbar-header:nth-child(1) > a.navbar-brand',
+    home: 'div#navbar>ul.nav.navbar-nav>li:nth-child(1)',
+    gettingStarted: 'div#navbar>ul.nav.navbar-nav>li:nth-child(2)',
+    developerGuide: 'div#navbar>ul.nav.navbar-nav>li:nth-child(3)',
+    apiReferences: 'div#navbar>ul.nav.navbar-nav>li:nth-child(4)',
+    blog: 'div#navbar>ul.nav.navbar-nav>li:nth-child(5)',
+    contact: 'div#navbar>ul.nav.navbar-nav>li:nth-child(6)'
+}
